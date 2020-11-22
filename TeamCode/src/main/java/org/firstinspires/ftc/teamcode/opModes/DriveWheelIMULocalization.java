91733549d3b072188238c8b8144c7013130a7a25
Radagast-the-infinite <ng.kyle101@gmail.com>
2020-11-21 22:59:44 -0800

    Merge branch 'dev' of /Users/JustKyle-ngaround/StudioProjects/UltimateOfficialV2 with conflicts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/DriveWheelIMULocalization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/DriveWheelIMULocalization.java
@@ -36,6 +36,7 @@ public class DriveWheelIMULocalization extends LinearOpMode {
         odometry.start();
 
         while (opModeIsActive()){
+            telemetry.addData("IMU Position", imu.getHeading());
             telemetry.addData("Odometry Position", odometry.getPosition());
             telemetry.update();
 
@@ -47,12 +48,12 @@ public class DriveWheelIMULocalization extends LinearOpMode {
 
     Odometry defaultConfiguration(){
         //physical wheels
-        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        DcMotor frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        DcMotor backRight = hardwareMap.get(DcMotor.class, "backRight");
-        DcMotor backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+//        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+//        DcMotor frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+//        DcMotor backRight = hardwareMap.get(DcMotor.class, "backRight");
+//        DcMotor backLeft = hardwareMap.get(DcMotor.class, "backLeft");
 //        DcMotor vertical = hardwareMap.get(DcMotor.class, "vertical");
-        DcMotor horizontal = hardwareMap.get(DcMotor.class, "intake");
+        DcMotor horizontal = hardwareMap.get(DcMotor.class, "horizontal");
 
         //odometry wheels
         OdometryWheel frontRightOdo = new DriveWheelOdometryWheel(new pose(178.5,168,Math.PI/2), frontRight);
@@ -60,7 +61,7 @@ public class DriveWheelIMULocalization extends LinearOpMode {
         OdometryWheel backRightOdo = new DriveWheelOdometryWheel(new pose(178.5,-168,Math.PI/2), backRight);
         OdometryWheel backLeftOdo = new DriveWheelOdometryWheel(new pose(-178.5,-168,Math.PI/2), backLeft);
 //        OdometryWheel verticalOdo = new FreeSpinOdoWheel(new pose(-180,91,Math.PI/2), vertical);
-        OdometryWheel horizontalOdo = new FreeSpinOdoWheel(new pose(170,-190,  Math.PI), horizontal);
+        OdometryWheel horizontalOdo = new FreeSpinOdoWheel(new pose(170,-190,0), horizontal);
 
         List<OdometryWheel> odometryWheels = new ArrayList<>();
         odometryWheels.add(frontLeftOdo);
@@ -72,6 +73,6 @@ public class DriveWheelIMULocalization extends LinearOpMode {
 
         // odometry system
         pose initial = new pose(0,0,Math.PI/2);
-        return new OdometryThatusesIMUforHeading(imu, initial, odometryWheels, telemetry);
+        return new OdometryThatusesIMUforHeading(imu, initial, odometryWheels);
     }
 }
