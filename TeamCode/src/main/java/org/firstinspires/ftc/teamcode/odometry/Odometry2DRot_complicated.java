89a23a542622686ab1ddc0a97125628c42f0bcf5
Radagast-the-infinite <ng.kyle101@gmail.com>
2020-11-25 16:41:36 -0800

    working translation for getPosition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/Odometry2DRot_complicated.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/Odometry2DRot_complicated.java
@@ -1,11 +1,14 @@
 
 package org.firstinspires.ftc.teamcode.odometry;
 
+import android.util.Log;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.GivesPosition;
 import org.firstinspires.ftc.teamcode.imu.IMU;
-import org.firstinspires.ftc.teamcode.odometry.OdoWheel2DRot;
 import org.firstinspires.ftc.teamcode.utility.pose;
+
+import java.util.ArrayList;
 
 
 public class Odometry2DRot_complicated implements GivesPosition {
@@ -13,11 +16,12 @@ public class Odometry2DRot_complicated implements GivesPosition {
     OdoWheel2DRot[]  wheels;
     IMU imu;
 
-    pose previousPosition;
-    volatile pose currentPosition; //Todo check if this needs to be volatile
+    pose previousPosition = new pose(0,0,0);
+    volatile pose currentPosition = new pose(0,0,0); //Todo check if this needs to be volatile
 
-    pose sumDeltaPosition = new pose (0,0,0);
-    pose actualDeltaPosition = new pose(0,0,0);
+    pose currentDelta = new pose(0,0,0);
+    pose totalTicks = new pose (0,0,0);
+    pose sumPrevious = new pose(0,0,0);
 
 //    private pose[] previousDeltaPositions;
 
@@ -30,7 +34,13 @@ public class Odometry2DRot_complicated implements GivesPosition {
 
     private final Thread loop = new Thread(() -> {
         while (running){
-           updateDelta(wheels);
+           updateTotalDelta(wheels);
+           updateCurentDelta();
+//            telemetry.addData("Total Wheel Displacement", totalTicks);
+//           telemetry.addData("current delta ", currentDelta);
+           setExtrinsic(currentDelta);
+           setExtrinsic(getCurrentIntrinsic());
+           telemetry.addData("Current Position", currentPosition);
         }
     });
 
@@ -57,44 +67,62 @@ public class Odometry2DRot_complicated implements GivesPosition {
                 * 2 * Math.PI * wheel.radius() * -1; //values are negative because of the way it is mounted
     }
 
-    void updateDelta(OdoWheel2DRot[] wheels) { //TODO take into account of all mounting positions
+    void updateTotalDelta(OdoWheel2DRot[] wheels) { //TODO take into account of all mounting positions
         for (OdoWheel2DRot wheel : wheels) {
             if (wheel.mountAngle == horizontal) {
-                sumDeltaPosition.x = ticksToDistance(wheel);
+                totalTicks.x = ticksToDistance(wheel);
             } else if (wheel.mountAngle == vertical) {
-                sumDeltaPosition.y = ticksToDistance(wheel);
+                totalTicks.y = ticksToDistance(wheel);
             }
-            telemetry.addData("Total Wheel Displacement", sumDeltaPosition);
-            telemetry.addData("Interval Wheel Displacement", actualDeltaPosition);
 
         }
+        totalTicks.r = imu.getHeading();
+    }
+    // for debugging
+//    ArrayList<Double> listOfXDelta = new ArrayList<Double>();
+
+    void updateCurentDelta(){
+        currentDelta.x = totalTicks.x - sumPrevious.x;
+//        listOfXDelta.add(currentDelta.x);
+
+//        Log.e("Interval Wheel Displacement", listOfXDelta.toString());
+        sumPrevious.x += currentDelta.x;
+
+        currentDelta.y = totalTicks.y - sumPrevious.y;
+        sumPrevious.y += currentDelta.y;
+
+        // gyroscope is special, everything is absolute not a sum, but we keep the same poses as before
+        // to make things easier to calculate
+        currentDelta.r = totalTicks.r - sumPrevious.r;
+        sumPrevious.r = currentDelta.r; // not adding just re-setting value
     }
 
-
-//    pose getCurrentIntrinsic(){
-//        // add the deltas to the previous position
-//        pose p = previousPosition; //make a copy of previous position so we can use it later to find extrinsic
-//        p.x += currentDeltaPosition.x;
-//        p.y += currentDeltaPosition.y;
-//        p.r = currentDeltaPosition.r;
-//        return p; //note that this is intrinsic
-//    }
+    pose getCurrentIntrinsic(){
+        // add the deltas to the previous position
+        pose p = previousPosition;
+        p.x += currentDelta.x;
+        p.y += currentDelta.y;
+        p.r = currentDelta.r;
+        return p; //note that this is intrinsic
+    }
 
 //    void resetDeltaPosition(){
 //        sumDeltaPosition = currentDeltaPosition;
 //    }
 
-//    void setExtrinsic(pose intrinsic){
-//        intrinsic = getCurrentIntrinsic();
-//        // previousPosition is currently intrinsic, so we need to rotate it to make it extrinsic
-//        currentPosition.x  = ((intrinsic.x - previousPosition.x) * Math.cos(currentDeltaPosition.r))
-//                - ((intrinsic.y - previousPosition.y) * Math.sin(currentDeltaPosition.r))
-//                + previousPosition.x;
-//
-//        currentPosition.y = ((intrinsic.x - previousPosition.x) * Math.sin(currentDeltaPosition.r))
-//                - ((intrinsic.y - previousPosition.y) * Math.cos(currentDeltaPosition.r))
-//                + previousPosition.y;
-//    }
+    void setExtrinsic(pose intrinsic){
+        // previousPosition is currently intrinsic, so we need to rotate it to make it extrinsic
+        currentPosition.x  = ((intrinsic.x - previousPosition.x) * Math.cos(currentDelta.r))
+                - ((intrinsic.y - previousPosition.y) * Math.sin(currentDelta.r))
+                + previousPosition.x;
+
+        currentPosition.y = ((intrinsic.x - previousPosition.x) * Math.sin(currentDelta.r))
+                - ((intrinsic.y - previousPosition.y) * Math.cos(currentDelta.r))
+                + previousPosition.y;
+
+        // the delta angle has been used above for calculations, now turn it back to absolute angle
+        currentPosition.r = imu.getHeading();
+    }
 
 
     @Override
