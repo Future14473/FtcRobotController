caf790501067d3a5ea92eeb075f88f2ed80b8136
Radagast-the-infinite <ng.kyle101@gmail.com>
2020-12-05 14:51:35 -0800

    Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Teleop.java
@@ -1,68 +1,55 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.imu.IMU;
 import org.firstinspires.ftc.teamcode.movement.Mecanum;
 import org.firstinspires.ftc.teamcode.utility.RotationUtil;
 
 
-@TeleOp(name="BaseDrive", group="Iterative Opmode")
+@TeleOp(name="BaseDrive", group="Teleop")
 //@Disabled
 //use DriveWheelIMULocalization for the same functionality instead
-public class BaseDrive extends OpMode
+public class Teleop extends LinearOpMode
 {
     // Declare OpMode members.
     Mecanum MecanumDrive;
     DcMotor intake;
     DcMotor taco;
+    DcMotor shooter_adjuster;
+    CRServo gate;
     IMU imu;
-    DcMotor shooter;
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
-    @Override
-    public void init() {
-        telemetry.addData("Status", "Initialized");
+
+
+
+    public void runOpMode() throws InterruptedException {
+
+        imu = new IMU(hardwareMap, telemetry);
 
         MecanumDrive = new Mecanum(hardwareMap);
+
         intake = hardwareMap.get(DcMotor.class, "intake");
         taco = hardwareMap.get(DcMotor.class, "taco");
-        shooter = hardwareMap.get(DcMotor.class, "shooter");
-        shooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        gate = hardwareMap.get(CRServo.class, "gate");
 
-        // Tell the driver that initialization is complete.
+        shooter_adjuster = hardwareMap.get(DcMotor.class, "shooter_adjuster");
+        shooter_adjuster.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        shooter_adjuster.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
         telemetry.addData("Status", "Initialized");
-    }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
+        waitForStart();
+        while (opModeIsActive()){
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
 
-    }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
-    @Override
-    public void loop() {
         // stop when no one is touching anything
 
         double y = -gamepad1.right_stick_y;
@@ -79,40 +66,40 @@ public class BaseDrive extends OpMode
         // make the intake do the correct trigger, + is outward, - is inward
         intake.setPower(-(intakeIn + intakeOut));
         taco.setPower(intakeIn + intakeOut);
-//        if (intakeIn != 0){
-//            intake.setPower(intakeIn);
-//        } else if(intakeOut != 0){
-//            intake.setPower(intakeOut);
-//        }
 
-        int shooterPosition = shooter.getCurrentPosition();
+
+        // shooter adjuster
         if (gamepad1.dpad_up){ // moves the target position 10 up
-            shooter.setTargetPosition(
-                    shooterPosition + 10
+            shooter_adjuster.setTargetPosition(
+                    shooter_adjuster.getCurrentPosition() + 10
             );
         } else if (gamepad1.dpad_down){
-            shooter.setTargetPosition(
-                    shooterPosition - 10
+            shooter_adjuster.setTargetPosition(
+                    shooter_adjuster.getTargetPosition() - 10
             );
         }
+
+        // shooter gate, 0 to 1, 0.5 is stationary
+        if (gamepad1.x){
+            gate.setPower(1);
+        } else if (gamepad1.y){
+            gate.setPower(0);
+        }
+        gate.setPower(0.5);
+
 
         MecanumDrive.drive(x/3, y/3,
                 (magnitude > 0.5 && Math.abs(turnPwr) > 0.08)? turnPwr:0);
 
 
         // Show the elapsed game time and wheel power.
-        telemetry.addData("Trigger Left: ", intakeOut);
+        telemetry.addData("Shooter Adjuster: ", shooter_adjuster.getCurrentPosition());
         telemetry.addData("Trigger Right: ", intakeIn);
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
-    }
 
-}
+
+}}
 
 
 
