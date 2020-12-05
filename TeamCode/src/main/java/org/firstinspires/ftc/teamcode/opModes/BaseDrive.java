0863731c9e2b891233fbcc1c7a93b774d6543ca8
Radagast-the-infinite <ng.kyle101@gmail.com>
2020-12-05 11:29:59 -0800

    Shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/BaseDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/BaseDrive.java
@@ -23,6 +23,7 @@ public class BaseDrive extends OpMode
     DcMotor intake;
     DcMotor taco;
     IMU imu;
+    DcMotor shooter;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -34,6 +35,9 @@ public class BaseDrive extends OpMode
         MecanumDrive = new Mecanum(hardwareMap);
         intake = hardwareMap.get(DcMotor.class, "intake");
         taco = hardwareMap.get(DcMotor.class, "taco");
+        shooter = hardwareMap.get(DcMotor.class, "shooter");
+        shooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -63,9 +67,12 @@ public class BaseDrive extends OpMode
 
         double y = -gamepad1.right_stick_y;
         double x = gamepad1.right_stick_x;
+
+        // absolute turning
         double targetDir = -Math.atan2(gamepad1.left_stick_y,gamepad1.left_stick_x) - Math.PI/2;
         double magnitude = Math.hypot(gamepad1.left_stick_y,gamepad1.left_stick_x);
         double turnPwr = RotationUtil.turnLeftOrRight(imu.getHeading(), targetDir, Math.PI * 2);
+
         double intakeOut = gamepad1.right_trigger;
         double intakeIn = -gamepad1.left_trigger;
 
@@ -77,6 +84,17 @@ public class BaseDrive extends OpMode
 //        } else if(intakeOut != 0){
 //            intake.setPower(intakeOut);
 //        }
+
+        int shooterPosition = shooter.getCurrentPosition()
+        if (gamepad1.dpad_up){ // moves the target position 10 up
+            shooter.setTargetPosition(
+                    shooterPosition + 10
+            );
+        } else if (gamepad1.dpad_down){
+            shooter.setTargetPosition(
+                    shooterPosition - 10
+            );
+        }
 
         MecanumDrive.drive(x/3, y/3,
                 (magnitude > 0.5 && Math.abs(turnPwr) > 0.08)? turnPwr:0);
